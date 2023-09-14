7e3cbaa9ffa7b351e515fce96afb6955215f42ee
Devohka <110058157+Devohka@users.noreply.github.com>
2023-09-14 20:15:34 +0300

    nnk

--- js/task-1_hw-22.js
+++ js/task-1_hw-22.js
@@ -1,29 +1,29 @@
 // 1
 
-// class Account {
+class Account {
 
-//     constructor({ login, email }) {
-//         this.logiN = login;
-//         this.emaiL = email;
-//     }
-//     getInfo() {
-//         return `${this.login} ${this.email}`;
-//     };
+    constructor({ login, email }) {
+        this.logiN = login;
+        this.emaiL = email;
+    }
+    getInfo() {
+        return `${this.login} ${this.email}`;
+    };
 
-// };
+};
 
-// const mango = new Account({
-//     login: 'Mangozedog',
-//     email: 'mango@dog.woof',
-// });
-// mango.getInfo(mango.login, mango.email); // Login: Mangozedog, Email: mango@dog.woof
-// console.log(mango);
+const mango = new Account({
+    login: 'Mangozedog',
+    email: 'mango@dog.woof',
+});
+mango.getInfo(mango.login, mango.email); // Login: Mangozedog, Email: mango@dog.woof
+console.log(mango);
 
-// const poly = new Account({
-//     login: "Poly",
-//     email: "poly@mail.com",
-// });
-// getInfo(poly.login, poly.email);
-// poly.getInfo(); // Login: Poly, Email: poly@mail.com
-// console.log(poly);
+const poly = new Account({
+    login: "Poly",
+    email: "poly@mail.com",
+});
+getInfo(poly.login, poly.email);
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
+console.log(poly);
 
